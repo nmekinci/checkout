68847eea1935e247d2074669577b0005b73fa6f9
Must <emailme>
2023-06-11 14:31:35 +0200

    .

--- app.js
+++ app.js
@@ -4,29 +4,25 @@
 // ******selectors
 
 
-// const main = document.querySelector(".main__product")
 const main = document.getElementById("products-preview")
 document.getElementsByClassName
 
+
+// ******** events
 main.addEventListener("click", (e) => {
-    // console.log(e.target.classList);
+ 
     if(e.target.classList.contains("fa-minus")) {
         if(e.target.nextElementSibling.innerText > 1) {
             e.target.nextElementSibling.innerText --
+            
         }        
     } else if (e.target.classList.contains("fa-plus")) {
         e.target.previousElementSibling.innerText ++;
-    
         e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText
-        
-        
 
-
-        // e.target.previousElementSibling.innerText
-        // e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".dollar").innerText
     } else if (e.target.classList.contains("fa-trash-can")){
-    //    e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
        e.target.closest(".main__product").remove()
+
     }
     
 })
