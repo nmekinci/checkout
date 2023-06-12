34b0f52df4720d6c2486c0cf7ad5dc3b654db712
Must <emailme>
2023-06-12 14:40:37 +0200

    selected products area

--- app.js
+++ app.js
@@ -5,19 +5,25 @@
 
 
 const main = document.getElementById("products-preview")
-document.getElementsByClassName
 
+
+const amountOfProducts = document.querySelector(".amount-of-product")
+
+const selectedProducts = document.querySelector(".main__sum-price")
+
+// let totalAmount = 0
 
 // ******** events
+
+
 main.addEventListener("click", (e) => {
     
-
-
     if(e.target.classList.contains("fa-minus")) {
         if(e.target.nextElementSibling.innerText > 1) {
             e.target.nextElementSibling.innerText --    
 
-            e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)   
+            e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)  
+            
         }        
     } else if (e.target.classList.contains("fa-plus")) {
         e.target.previousElementSibling.innerText ++;
@@ -26,8 +32,27 @@ main.addEventListener("click", (e) => {
 
     } else if (e.target.classList.contains("fa-trash-can")){
         if (confirm("are you sure???") == true){
-       e.target.closest(".main__product").remove()
+       e.target.closest(".main__product").remove();
+
+       amountOfProducts.innerText --
+        
     }else {}
     }
+    selectedProducts.innerHTML = updateAmount()
     
 })
+
+// function subTotal(position) {
+//     e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.{position}.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+// }
+
+function updateAmount () {
+    let totalAmount = document.getElementsByClassName("main__product-line-price");
+    // selectedProducts.innerText = totalAmount
+    let sum = 0;
+    for(let i of totalAmount){
+         sum += +(i.innerText); //sum = sum + Number(i.innerText)
+        // console.log(i.innerText);
+    }
+    return sum;
+}
