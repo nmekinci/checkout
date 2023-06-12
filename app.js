6a5b26cd98a8e84cb7757e14421bb646ad64aa59
Must <emailme>
2023-06-12 13:09:19 +0200

    main product area complated

--- app.js
+++ app.js
@@ -10,19 +10,24 @@ document.getElementsByClassName
 
 // ******** events
 main.addEventListener("click", (e) => {
- 
+    
+
+
     if(e.target.classList.contains("fa-minus")) {
         if(e.target.nextElementSibling.innerText > 1) {
-            e.target.nextElementSibling.innerText --
-            
+            e.target.nextElementSibling.innerText --    
+
+            e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)   
         }        
     } else if (e.target.classList.contains("fa-plus")) {
         e.target.previousElementSibling.innerText ++;
-        e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText
+
+        e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
 
     } else if (e.target.classList.contains("fa-trash-can")){
+        if (confirm("are you sure???") == true){
        e.target.closest(".main__product").remove()
-
+    }else {}
     }
     
 })
